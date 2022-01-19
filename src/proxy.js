18c8c5fd89cc382eb72b971a42c99babc06638f7
rsgilbert <gssenyonjo@gmail.com>
2022-01-19 10:27:09 +0300

    changed to sgms-api proxy

--- src/proxy.js
+++ src/proxy.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 router.use('*', createProxyMiddleware({ 
-    target: 'https://api.beyonic.com',
+    target: 'http://41.198.122.46:5000',
     logLevel: 'debug',
     changeOrigin: true
  }))
